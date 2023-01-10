0e13f5dbd2c6b787f4826f3fcf740a5517ecf609
Wojciech Brusilo <wojciech.brusilo@wp.pl>
2023-01-10 18:52:55 +0100

    computerMove warianty

--- js/script.js
+++ js/script.js
@@ -4,7 +4,28 @@ var computerMove;
 computerMove = 'kamień';
 printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
 
+var computerMove;
+computerMove = 'papier';
+printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to nożyce, to wygrywasz!');
+
+var computerMove;
+computerMove = 'nożyce';
+printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to kamień, to wygrywasz!');
+
 var playerMove;
 computerMove = 'papier';
 printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to kamień, to wygrywasz!');
 
+var computerMove, randomNumber;
+randomNumber = Math.floor(Math.random() * 3 + 1);
+console.log('wylosowana liczba to: ' + randomNumber);
+if (randomNumber == '1') {
+  computerMove = 'kamień';
+} else if (randomNumber == '2') {
+    computerMove = 'nożyce';
+} else if (randomNumber == '3') {
+    computerMove = 'papier';
+} else {
+  computerMove = 'nieznany ruch';
+}
+printMessage('Mój ruch: ' + computerMove);
