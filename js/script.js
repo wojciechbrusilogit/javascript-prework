af17c857d9e649d991b72e6cb4938d9a87700e77
Wojciech Brusilo <wojciech.brusilo@wp.pl>
2023-01-10 19:30:34 +0100

    Wynik gry przed usunięciem starego kodu

--- js/script.js
+++ js/script.js
@@ -47,3 +47,53 @@ if (playerInput == '1') {
   playerMove = 'kamień';
 }
 printMessage('Twój ruch: ' + playerMove);
+
+//Wynik gry
+
+var argMoveId, argPlayerMove, argComputerMove, computerMove, playerMove, randomNumber, playerInput;
+
+/**
+ * Wynik gry 1/2...
+ */
+function getMoveName(argMoveId) {
+  console.log('wywołano funkcję getMoveName z argumentem: ' + argMoveId);
+  if (argMoveId == 1) {
+    return 'kamień';
+  } else if (argMoveId == 2) {
+    return 'nożyce';
+  } else if (argMoveId == 3) {
+    return 'papier'
+  } else {
+    printMessage('Nie znam ruchu o id ' + argMoveId + '. Zakładam, że chodziło o "kamień".');
+    return 'kamień';
+  }
+}
+
+/**
+ * Wynik gry 2/2...
+ */
+function displayResult(argPlayerMove, argComputerMove) {
+  console.log('wywołano funkcję displayResults z argumentami: ' + argPlayerMove + ', ' + argComputerMove);
+  if (argPlayerMove == 'papier' && argComputerMove == 'kamień') {
+    printMessage('Wygrywasz!');
+  } else if (argPlayerMove == 'kamień' && argComputerMove == 'nożyce') {
+    printMessage('Wygrywasz!');
+  } else if (argPlayerMove == 'nożyce' && argComputerMove == 'papier') {
+    printMessage('Wygrywasz!');
+  } else if (argPlayerMove == 'playerMove' && argComputerMove == 'computerMove') {
+    printMessage('Remis!');
+  } else {
+    printMessage('Przegrywasz :(');
+  } 
+  printMessage('Zagrałem ' + argComputerMove + ', a Ty ' + argPlayerMove);
+  
+}
+playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+console.log('wybór ruchu gracza to: ' + playerInput);
+playerMove = getMoveName(playerInput);
+console.log('ruch gracza to: ' + playerMove);
+randomNumber = Math.floor(Math.random() * 3 + 1);
+console.log('wylosowana liczba to: ' + randomNumber);
+computerMove = getMoveName(randomNumber);
+console.log('ruch komputera to: ' + computerMove);
+displayResult(playerMove, computerMove);
