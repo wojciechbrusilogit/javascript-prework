c30d6c82ce0fbe82262d57862f2d01070fc85b8b
Wojciech Brusilo <wojciech.brusilo@wp.pl>
2023-01-10 18:37:24 +0100

    playerMove

---
+++ js/script.js
@@ -0,0 +1,10 @@
+printMessage('Zagrałem ' + 'kamień' + '! Jeśli Twój ruch to papier, to wygrywasz!');
+
+var computerMove;
+computerMove = 'kamień';
+printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+
+var playerMove;
+computerMove = 'papier';
+printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to kamień, to wygrywasz!');
+
